c81374347814141242718bed47ae748e741240bc
Douglas Volcato <douglasvolcato@gmail.com>
2022-12-22 00:49:37 -0300

    feat: implement homepage

--- src/main/react-adapter/App.tsx
+++ src/main/react-adapter/App.tsx
@@ -1,9 +1,37 @@
-function App() {
+import "../../presentation/styles/header.css";
+import "../../presentation/styles/input.css";
+import "../../presentation/styles/button.css";
+import "../../presentation/styles/paragraph.css";
+import { useEffect, useState } from "react";
+import { Header } from "../../presentation/components/header";
+import { Input } from "../../presentation/components/input";
+import { Button } from "../../presentation/components/button";
+import { Paragraph } from "../../presentation/components/paragraph";
+
+export function App() {
+  const [state, setState] = useState<any>({
+    inputValue: "teste",
+  });
+
+  useEffect(() => {
+    const script = document.getElementById("myScript")?.innerHTML;
+    if (script) {
+      window.eval(script);
+    }
+
+    document
+      .getElementById("mainButton")
+      ?.addEventListener("click", function () {
+        setState({ inputValue: document.getElementById("main-input")?.value });
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <p>Main project</p>
+    <div>
+      <div dangerouslySetInnerHTML={{ __html: Header("Homepage") }} />;
+      <div dangerouslySetInnerHTML={{ __html: Input() }} />;
+      <div dangerouslySetInnerHTML={{ __html: Button() }} />;
+      <div dangerouslySetInnerHTML={{ __html: Paragraph(state.inputValue) }} />;
     </div>
   );
 }
-
-export default App;

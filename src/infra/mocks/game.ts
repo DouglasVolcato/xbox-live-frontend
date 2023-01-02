45971df548b0d9ca720418e755bc93e9844fffe8
Douglas Volcato <douglasvolcato@gmail.com>
2023-01-02 19:22:33 -0300

    feat: add mock entities

---
+++ src/infra/mocks/game.ts
@@ -0,0 +1,15 @@
+import { Game } from "../../domain/game";
+
+export const mockedGame: Game = {
+  id: "234d234123d15d1",
+  title: "Chess",
+  coverImageUrl: "https://images.pexels.com/photos/260024/pexels-photo-260024.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  description: "No description needed",
+  year: 50,
+  imdbScore: 10,
+  trailerYouTubeUrl: "https://www.youtube.com/watch?v=LShRoP7blVs",
+  gameplayYouTubeUrl: "https://www.youtube.com/watch?v=LShRoP7blVs",
+  userId: "38dfger3278dgdfg2348dfg47",
+  createdAt: "2023-01-02",
+  updatedAt: "2023-01-02",
+};

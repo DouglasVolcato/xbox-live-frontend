d75bd66a809e7c5fd893b073545e5f4523482fad
Douglas Volcato <douglasvolcato@gmail.com>
2023-01-03 23:54:41 -0300

    feat: add enums

--- src/presentation/components/button.ts
+++ src/presentation/components/button.ts
@@ -1,19 +1,18 @@
-import { HtmlElement } from "../Helpers/html-element-helper";
+import { HtmlElement } from "../helpers/html-element";
 import { ComponentInterface } from "../abstract/component-interface";
+import { ButtonTypeEnum } from "../enums/button/button-type-enum";
 
 export class Button extends HtmlElement implements ComponentInterface {
   private readonly name: string;
   private readonly type: string;
-  private readonly className: string;
 
-  constructor(name: string, type: string, className: string, id: string) {
+  constructor(name: string, type: ButtonTypeEnum, id: string) {
     super(id);
     this.name = name;
     this.type = type;
-    this.className = className;
   }
 
   render(): string {
-    return `<button id="${this.id}" class="${this.className}" type="${this.type}">${this.name}</button>`;
+    return `<button id="${this.id}" class="button" type="${this.type}">${this.name}</button>`;
   }
 }

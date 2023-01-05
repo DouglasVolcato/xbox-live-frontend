cbde890ddc5f81cd9b11aad6262108f96e64d377
Douglas Volcato <douglasvolcato@gmail.com>
2023-01-04 23:43:02 -0300

    feat: implement input class

--- src/presentation/components/input.ts
+++ src/presentation/components/input.ts
@@ -5,16 +5,23 @@ import { InputTypeEnum } from "../enums/input/input-type-enum";
 export class Input extends HtmlElement implements ComponentInterface {
   private readonly type: string;
   private readonly value: string;
+  private readonly placeholder: string;
 
-  constructor(type: InputTypeEnum, value: string, id: string) {
+  constructor(
+    type: InputTypeEnum,
+    value: string,
+    id: string,
+    placeholder = ""
+  ) {
     super(id);
     this.type = type;
     this.value = value;
+    this.placeholder = placeholder;
   }
 
   render(): string {
     return `
-        <input class="input" id="${this.id}" type="${this.type}" value="${this.value}" />
+        <input class="input" id="${this.id}" type="${this.type}" value="${this.value}" placeholder="${this.placeholder}" />
     `;
   }
 }

8ca8a55a71b387a19624dbb3905874b90e667026
Douglas Volcato <douglasvolcato@gmail.com>
2023-01-07 17:37:22 -0300

    fix: fix div flex enums

--- src/presentation/components/div.ts
+++ src/presentation/components/div.ts
@@ -1,45 +1,44 @@
 import { ComponentInterface } from "../abstract/component-interface";
 import { DivTypeEnum } from "../enums/div/div-type-enum";
 import { FlexDirectionEnum } from "../enums/div/flex-direction-enum";
-import { FlexTypeEnum } from "../enums/div/flex-type-enum";
+import { FlexJustificationEnum } from "../enums/div/flex-justification-enum";
 import { ComponentComposer } from "../helpers/composers/component-composer";
 import { HtmlElement } from "../helpers/html/html-element";
 
 export class Div extends HtmlElement implements ComponentInterface {
   private readonly divType: DivTypeEnum;
-  private readonly components: ComponentInterface[];
-  private readonly flexType: string;
   private readonly flexDirection: string;
+  private readonly flexJustification: string;
+  private readonly components: ComponentInterface[];
 
   constructor(
     divType: DivTypeEnum,
-    flexType: FlexTypeEnum,
     flexDirection: FlexDirectionEnum,
+    flexJustification: FlexJustificationEnum,
     components: ComponentInterface[],
     id: string,
     classes: string[] = []
   ) {
     super(id, classes);
     this.divType = divType;
-    this.components = components;
-    this.flexType = flexType;
     this.flexDirection = flexDirection;
+    this.flexJustification = flexJustification;
+    this.components = components;
   }
 
   render(): string {
-    const flexType = this.getFlexType(this.flexType);
     const flexDirection = this.getFlexDirection(this.flexDirection);
+    const flexJustification = this.getFlexJustification(this.flexJustification);
     const components = new ComponentComposer(this.components);
+    const classes = `div flex-body ${flexJustification} ${flexDirection} ${this.classes}`;
     return `
-        <${this.divType} id="${
-      this.id
-    }" class="div ${flexType} ${flexDirection} ${this.classes}">
+        <${this.divType} id="${this.id}" class="${classes}">
             ${components.compose()}
         </${this.divType}>
     `;
   }
 
-  private getFlexType(type: string): string {
+  private getFlexDirection(type: string): string {
     switch (type.toLowerCase()) {
       case "column":
         return "flex-d-column";
@@ -50,7 +49,7 @@ export class Div extends HtmlElement implements ComponentInterface {
     }
   }
 
-  private getFlexDirection(direction: string): string {
+  private getFlexJustification(direction: string): string {
     switch (direction.toLowerCase()) {
       case "evenly":
         return "justify-evenly";

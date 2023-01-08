4854c7df23a8ad320dc8aa7fc8b51de9bfb17e34
Douglas Volcato <douglasvolcato@gmail.com>
2023-01-07 21:25:52 -0300

    feat: implement homepage

--- src/main/factories/home-page-factory.ts
+++ src/main/factories/home-page-factory.ts
@@ -1,21 +1,51 @@
 import { mockedGame } from "../../infra/mocks/game";
+import { HomePage } from "../../presentation/pages/home-page";
 import { Div } from "../../presentation/components/div";
 import { Header } from "../../presentation/components/header";
 import { Image } from "../../presentation/components/image";
 import { Paragraph } from "../../presentation/components/paragraph";
-import { Title } from "../../presentation/components/title";
 import { DivTypeEnum } from "../../presentation/enums/div/div-type-enum";
 import { FlexDirectionEnum } from "../../presentation/enums/div/flex-direction-enum";
 import { FlexJustificationEnum } from "../../presentation/enums/div/flex-justification-enum";
-import { HomePage } from "../../presentation/pages/home-page";
+import { mockedUser } from "../../infra/mocks/user";
+import { mockedProfile } from "../../infra/mocks/profile";
 
 const { DIV } = DivTypeEnum;
 const { COLUMN, ROW } = FlexDirectionEnum;
 const { BETWEEN, EVENLY } = FlexJustificationEnum;
 
 export function makeHomePageFactory(): HomePage {
-  const title = new Title("HomePage", "homepage-title");
-  const header = new Header([title], "homepage-header");
+  const userName = new Paragraph(
+    mockedUser.name,
+    "homepage-header-userInfo-userName",
+    ["profileName"]
+  );
+  const userImage = new Image(
+    "User",
+    mockedProfile.imageUrl,
+    "homepage-header-userInfo-userImage",
+    ["profileImage"]
+  );
+  const userInfo = new Div(
+    DIV,
+    ROW,
+    EVENLY,
+    [userImage, userName],
+    "homepage-header-userInfo"
+  );
+
+  const clock = new Paragraph("10:20", "homepage-header-clock", ["clock"]);
+
+  const clockDiv = new Div(
+    DIV,
+    ROW,
+    EVENLY,
+    [clock],
+    "homepage-header-userInfo", ["clockDiv"]
+  );
+
+  const header = new Header([userInfo, clockDiv], "homepage-header");
+
   const gameTitle = new Paragraph(
     mockedGame.title,
     "homepage-gamesDiv-gameCard-gameTitle"
@@ -34,6 +64,7 @@ export function makeHomePageFactory(): HomePage {
     "homepage-gamesDiv-gameCard",
     ["homepage-gameCard", "background-dark-blue", "border-light-gray"]
   );
+
   const gamesDiv = new Div(
     DIV,
     ROW,

db10b5482f3502ab79958b8ae7845f0f88307889
Douglas Volcato <douglasvolcato@gmail.com>
2023-01-08 13:56:59 -0300

    feat: implement browser routes

--- src/main/react-adapter/App.tsx
+++ src/main/react-adapter/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, BrowserRouter, useNavigate } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { Styles } from "./components/styles";
 import { Homepage } from "./pages/home-page";
 
@@ -8,7 +8,7 @@ export function App() {
       <BrowserRouter>
         <Styles />
         <Routes>
-          <Route path="/" element={<Homepage />} />
+          <Route path="*" element={<Homepage />} />
         </Routes>
       </BrowserRouter>
     </div>

9f13b378843c9c1e8c0062428d7799e663c10eac
Douglas Volcato <douglasvolcato@gmail.com>
2023-01-13 00:33:28 -0300

    refactor: move app imports to react root render

--- src/main/react-adapter/main.tsx
+++ src/main/react-adapter/main.tsx
@@ -1,9 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Styles } from "./components/styles";
+import { FavoriteGames } from "./pages/favoriteGames-page";
+import { GameList } from "./pages/gameList-page";
+import { Homepage } from "./pages/home-page";
+import { LoggedUser } from "./pages/loggedUser-page";
+import { Login } from "./pages/login-page";
+import { Register } from "./pages/register-page";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Styles />
+      <Routes>
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="logged-profile" element={<LoggedUser />} />
+        <Route path="favorite-games" element={<FavoriteGames />} />
+        <Route path="game-list" element={<GameList />} />
+        <Route path="*" element={<Homepage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );

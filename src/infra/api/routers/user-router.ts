a23ba5dc39e235458f11e40c34bf9cb1b3b9ae3a
Douglas Volcato <douglasvolcato@gmail.com>
2023-01-15 18:05:40 -0300

    feat: add ap UserRouter

---
+++ src/infra/api/routers/user-router.ts
@@ -0,0 +1,67 @@
+import { User } from "../../../domain/user";
+import { ApiConection } from "../connection/apiConnection";
+import { ApiResponse } from "../dtos/apiResponse-dto";
+import { UserDto } from "../dtos/user-dto";
+import { TokenHandler } from "../../helpers/tokenHandler-helper";
+import { HttpRequestAdapter } from "../../../helpers/adapters/httpRequest-adapter";
+
+export class UserRouter extends ApiConection {
+  private readonly tokenHandler: TokenHandler;
+  private readonly httpRequestAdapter: HttpRequestAdapter;
+
+  constructor(
+    tokenHandler: TokenHandler,
+    httpRequestAdapter: HttpRequestAdapter
+  ) {
+    super();
+    this.tokenHandler = tokenHandler;
+    this.httpRequestAdapter = httpRequestAdapter;
+  }
+
+  public async create(user: UserDto): Promise<ApiResponse<User>> {
+    return await this.httpRequestAdapter.post(
+      this.apiLink + "/user/create-user",
+      user,
+      this.tokenHandler.getAuthorization()
+    );
+  }
+
+  public async getAll(): Promise<ApiResponse<User[]>> {
+    return await this.httpRequestAdapter.get(
+      this.apiLink + "/user/get-all-users",
+      this.tokenHandler.getAuthorization()
+    );
+  }
+
+  public async getById(userId: string): Promise<ApiResponse<User>> {
+    return await this.httpRequestAdapter.get(
+      this.apiLink + "/user/get-user-by-id/" + userId,
+      this.tokenHandler.getAuthorization()
+    );
+  }
+
+  public async getByEmail(userEmail: string): Promise<ApiResponse<User>> {
+    return await this.httpRequestAdapter.get(
+      this.apiLink + "/user/get-user-by-email/" + userEmail,
+      this.tokenHandler.getAuthorization()
+    );
+  }
+
+  public async delete(userId: string): Promise<ApiResponse<User>> {
+    return await this.httpRequestAdapter.delete(
+      this.apiLink + "/user/delete-user/" + userId,
+      this.tokenHandler.getAuthorization()
+    );
+  }
+
+  public async update(
+    userId: string,
+    user: UserDto
+  ): Promise<ApiResponse<User>> {
+    return await this.httpRequestAdapter.patch(
+      this.apiLink + "/user/update-user/" + userId,
+      user,
+      this.tokenHandler.getAuthorization()
+    );
+  }
+}

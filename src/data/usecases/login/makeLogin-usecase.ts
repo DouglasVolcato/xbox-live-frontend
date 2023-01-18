23a98848c7be7c46be147b14fda6edb7cb7c76db
Douglas Volcato <douglasvolcato@gmail.com>
2023-01-17 21:11:05 -0300

    feat: add token store after login

--- src/data/usecases/login/makeLogin-usecase.ts
+++ src/data/usecases/login/makeLogin-usecase.ts
@@ -1,12 +1,15 @@
 import { HtmlElement } from "../../../helpers/html/html-element";
 import { AuthRouter } from "../../../infra/api/routers/auth-router";
+import { TokenHandler } from "../../../infra/helpers/tokenHandler-helper";
 import { Service } from "../../abstract/service-interface";
 
 export class MakeLoginUseCase implements Service {
   private readonly authRouter: AuthRouter;
+  private readonly tokenHandler: TokenHandler;
 
-  constructor(authRouter: AuthRouter) {
+  constructor(authRouter: AuthRouter, tokenHandler: TokenHandler) {
     this.authRouter = authRouter;
+    this.tokenHandler = tokenHandler;
   }
 
   public execute(navigateCallbackFunction = () => {}): void {
@@ -21,6 +24,7 @@ export class MakeLoginUseCase implements Service {
         password: passwordInput.getValue(),
       });
       if (response.body.token) {
+        this.tokenHandler.storeToken(response.body.token);
         navigateCallbackFunction();
       }
     });
--- src/main/factories/controllers/login-controller-factory.ts
+++ src/main/factories/controllers/login-controller-factory.ts
@@ -1,13 +1,15 @@
 import { MakeLoginUseCase } from "../../../data/usecases/login/makeLogin-usecase";
 import { AuthRouter } from "../../../infra/api/routers/auth-router";
+import { TokenHandler } from "../../../infra/helpers/tokenHandler-helper";
 import { LoginController } from "../../../presentation/controllers/login-controller";
 import { HttpRequestAdapter } from "../../adapters/httpRequest-adapter";
 import { makeLoginPage } from "../pages/login-page-factory";
 
 export function makeLoginControllerFactory(): LoginController {
   const httpRequestAdapter = new HttpRequestAdapter();
+  const tokenHandler = new TokenHandler();
   const authRouter = new AuthRouter(httpRequestAdapter);
-  const makeLoginUseCase = new MakeLoginUseCase(authRouter);
+  const makeLoginUseCase = new MakeLoginUseCase(authRouter, tokenHandler);
   const loginPage = makeLoginPage();
   return new LoginController(loginPage, makeLoginUseCase);
 }

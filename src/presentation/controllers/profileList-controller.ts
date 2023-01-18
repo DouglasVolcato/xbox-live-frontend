141aa9db364ffa08c920f25e38d7a7f33eed5cbe
Douglas Volcato <douglasvolcato@gmail.com>
2023-01-18 01:32:01 -0300

    feat: add ProfileListController

---
+++ src/presentation/controllers/profileList-controller.ts
@@ -0,0 +1,24 @@
+import { GenerateProfileCardsUseCase } from "../../data/usecases/profile/generateProfileCards-usecase";
+import { ControllerInterface } from "../abstract/controller.interface";
+import { ProfileListPage } from "../pages/profileList-page";
+
+export class ProfileListController implements ControllerInterface {
+  private readonly profileListPage: ProfileListPage;
+  private readonly generateProfileCardsUseCase: GenerateProfileCardsUseCase;
+
+  constructor(
+    profileListPage: ProfileListPage,
+    generateProfileCardsUseCase: GenerateProfileCardsUseCase
+  ) {
+    this.profileListPage = profileListPage;
+    this.generateProfileCardsUseCase = generateProfileCardsUseCase;
+  }
+
+  public renderPage(): string {
+    return this.profileListPage.render();
+  }
+
+  public async updateProfileCards(): Promise<void> {
+    return await this.generateProfileCardsUseCase.execute();
+  }
+}

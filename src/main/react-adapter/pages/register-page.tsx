bf46a865ce14ea465f7255993b2ab9490e40b658
Douglas Volcato <douglasvolcato@gmail.com>
2023-01-17 21:04:43 -0300

    feat: add RegisterController factory

--- src/main/react-adapter/pages/register-page.tsx
+++ src/main/react-adapter/pages/register-page.tsx
@@ -1,7 +1,17 @@
-import { makeRegisterPage } from "../../factories/pages/register-page-factory";
+import { useEffect } from "react";
+import { makeRegisterControllerFactory } from "../../factories/controllers/register-controller-factory";
 import { HtmlComponent } from "../helpers/html-component-helper";
 
 export function Register() {
-  const registerPage = makeRegisterPage();
-  return <HtmlComponent component={registerPage.render()} />;
+  const registerPage = makeRegisterControllerFactory();
+
+  function teste() {
+    alert("User registered!");
+  }
+
+  useEffect(() => {
+    registerPage.registerUser(teste);
+  }, []);
+
+  return <HtmlComponent component={registerPage.renderPage()} />;
 }

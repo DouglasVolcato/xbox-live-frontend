9962071864945ab5d99e69b658ee135d9ebd1038
Douglas Volcato <douglasvolcato@gmail.com>
2023-01-20 00:43:15 -0300

    feat: add UserHandler class

--- src/main/factories/controllers/login-controller-factory.ts
+++ src/main/factories/controllers/login-controller-factory.ts
@@ -4,12 +4,18 @@ import { TokenHandler } from "../../../helpers/token/tokenHandler-helper";
 import { LoginController } from "../../../presentation/controllers/login-controller";
 import { HttpRequestAdapter } from "../../../helpers/adapters/httpRequest-adapter";
 import { makeLoginPage } from "../pages/login-page-factory";
+import { UserHandler } from "../../../helpers/user/userHandler-helper";
 
 export function makeLoginControllerFactory(): LoginController {
   const httpRequestAdapter = new HttpRequestAdapter();
   const tokenHandler = new TokenHandler();
+  const userHandler = new UserHandler();
   const authRouter = new AuthRouter(httpRequestAdapter);
-  const makeLoginUseCase = new MakeLoginUseCase(authRouter, tokenHandler);
+  const makeLoginUseCase = new MakeLoginUseCase(
+    authRouter,
+    tokenHandler,
+    userHandler
+  );
   const loginPage = makeLoginPage();
   return new LoginController(loginPage, makeLoginUseCase);
 }

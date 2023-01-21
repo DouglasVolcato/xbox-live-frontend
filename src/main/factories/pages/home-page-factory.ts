9b82b198660ff478ec3e2b9cd12e3d6db651e8ba
Douglas Volcato <douglasvolcato@gmail.com>
2023-01-21 01:00:40 -0300

    refactor: implement HomePage factory components

--- src/main/factories/pages/home-page-factory.ts
+++ src/main/factories/pages/home-page-factory.ts
@@ -1,11 +1,19 @@
 import { HomePage } from "../../../presentation/pages/home-page";
-import { makeGameListFactory } from "../components/gameList-component-factory";
 import { makeHeaderFactory } from "../components/header-component-factory";
 import { makeMenuFactory } from "../components/menu-component-factory";
+import { DivTypeEnum } from "../../../presentation/enums/div/div-type-enum";
+import { FlexDirectionEnum } from "../../../presentation/enums/div/flex-direction-enum";
+import { FlexJustificationEnum } from "../../../presentation/enums/div/flex-justification-enum";
+import { Div } from "../../../presentation/components/div";
 
+const { DIV } = DivTypeEnum;
+const { ROW } = FlexDirectionEnum;
+const { EVENLY } = FlexJustificationEnum;
 export function makeHomePageFactory(): HomePage {
   const header = makeHeaderFactory();
-  const gamesDiv = makeGameListFactory();
+  const gamesDiv = new Div(DIV, ROW, EVENLY, [], "homePage-gamesDiv", [
+    "flex-wrap",
+  ]);
   const menuDiv = makeMenuFactory();
 
   return new HomePage(header, gamesDiv, menuDiv);

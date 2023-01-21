7d0c82b3512aa881321a6335d207c864f0ab8281
Douglas Volcato <douglasvolcato@gmail.com>
2023-01-21 00:00:05 -0300

    refactor: remove mocked games

--- src/main/factories/components/gameList-component-factory.ts
+++ src/main/factories/components/gameList-component-factory.ts
@@ -1,41 +1,12 @@
-import { mockedGame } from "../../../infra/mocks/game";
 import { Div } from "../../../presentation/components/div";
-import { Image } from "../../../presentation/components/image";
-import { Paragraph } from "../../../presentation/components/paragraph";
 import { DivTypeEnum } from "../../../presentation/enums/div/div-type-enum";
 import { FlexDirectionEnum } from "../../../presentation/enums/div/flex-direction-enum";
 import { FlexJustificationEnum } from "../../../presentation/enums/div/flex-justification-enum";
 
 const { DIV } = DivTypeEnum;
-const { COLUMN, ROW } = FlexDirectionEnum;
+const { ROW } = FlexDirectionEnum;
 const { EVENLY } = FlexJustificationEnum;
 
 export function makeGameListFactory(): Div {
-  const gameTitle = new Paragraph(
-    mockedGame.title,
-    "homepage-gamesDiv-gameCard-gameTitle"
-  );
-  const gameImage = new Image(
-    "Game image",
-    mockedGame.coverImageUrl,
-    "homepage-gamesDiv-gameCard-gameImage",
-    ["gameCardImage"]
-  );
-  const gameCard = new Div(
-    DIV,
-    COLUMN,
-    EVENLY,
-    [gameImage, gameTitle],
-    "homepage-gamesDiv-gameCard",
-    ["homepage-gameCard", "background-dark-blue", "border-light-gray"]
-  );
-
-  return new Div(
-    DIV,
-    ROW,
-    EVENLY,
-    [gameCard, gameCard, gameCard, gameCard],
-    "homepage-gamesDiv",
-    ["flex-wrap"]
-  );
+  return new Div(DIV, ROW, EVENLY, [], "homepage-gamesDiv", ["flex-wrap"]);
 }

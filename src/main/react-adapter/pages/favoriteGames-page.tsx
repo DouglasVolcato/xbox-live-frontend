f51551f5c9b25a05c8c2caa9658c0be5de429eae
Douglas Volcato <douglasvolcato@gmail.com>
2023-01-21 00:28:09 -0300

    feat: add FavoriteGamesController factory

--- src/main/react-adapter/pages/favoriteGames-page.tsx
+++ src/main/react-adapter/pages/favoriteGames-page.tsx
@@ -1,7 +1,17 @@
-import { makeFavoriteGamesPageFactory } from "../../factories/pages/favoriteGames-page-factory";
+import { useEffect } from "react";
+import { makeFavoriteGamesControllerFactory } from "../../factories/controllers/favoriteGames-controller-factory";
 import { HtmlComponent } from "../helpers/html-component-helper";
 
 export function FavoriteGames() {
-  const favoriteGames = makeFavoriteGamesPageFactory();
-  return <HtmlComponent component={favoriteGames.render()} />;
+  const favoriteGamesController = makeFavoriteGamesControllerFactory();
+
+  function test() {
+    alert("Game selected!");
+  }
+
+  useEffect(() => {
+    favoriteGamesController.setGameList(test);
+  });
+
+  return <HtmlComponent component={favoriteGamesController.renderPage()} />;
 }

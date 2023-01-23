3e42f58715ba24860a7e8c5d121a0533bd0b95b8
Douglas Volcato <douglasvolcato@gmail.com>
2023-01-23 00:57:32 -0300

    feat: add ValidateTokenController factory

--- src/main/react-adapter/main.tsx
+++ src/main/react-adapter/main.tsx
@@ -3,33 +3,29 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { RouteComposer } from "./components/routes";
 import { StyleComposer } from "./components/styles";
 import { FavoriteGames } from "./pages/favoriteGames-page";
-import { GameCreationAdmin } from "./pages/gameCreationAdmin";
-import { GameEditionAdmin } from "./pages/gameEditionAdmin";
 import { GameList } from "./pages/gameList-page";
-import { GamesAdmin } from "./pages/gamesAdmin-page";
 import { GameView } from "./pages/gameView-page";
 import { Homepage } from "./pages/home-page";
-import { HomeAdmin } from "./pages/homeAdmin-page";
 import { LoggedUser } from "./pages/loggedUser-page";
 import { Login } from "./pages/login-page";
 import { ProfileCreation } from "./pages/profileCreation-page";
 import { ProfileList } from "./pages/profileList-page";
 import { Register } from "./pages/register-page";
+import { GamesAdmin } from "./pages/gamesAdmin-page";
+import { HomeAdmin } from "./pages/homeAdmin-page";
+import { GameCreationAdmin } from "./pages/gameCreationAdmin";
+import { GameEditionAdmin } from "./pages/gameEditionAdmin";
 import { UserEditionAdmin } from "./pages/userEditionAdmin-page";
 import { UsersAdmin } from "./pages/usersAdmin-page";
+import { Validator } from "./helpers/validator-helper";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>
     <StyleComposer />
     <RouteComposer />
+    <Validator />
     <Routes>
-      <Route path="/login" element={<Login />} />
-      {/* <Route path="home-admin" element={<HomeAdmin />} />
-      <Route path="games-admin" element={<GamesAdmin />} />
-      <Route path="users-admin" element={<UsersAdmin />} />
-      <Route path="user-edition-admin" element={<UserEditionAdmin />} />
-      <Route path="game-edition-admin" element={<GameEditionAdmin />} />
-      <Route path="game-creation-admin" element={<GameCreationAdmin />} /> */}
+      <Route path="login" element={<Login />} />
       <Route path="register" element={<Register />} />
       <Route path="profile-list" element={<ProfileList />} />
       <Route path="profile-creation" element={<ProfileCreation />} />
@@ -38,6 +34,13 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
       <Route path="game-list" element={<GameList />} />
       <Route path="game-view" element={<GameView />} />
       <Route path="home-page" element={<Homepage />} />
+
+      {/* <Route path="home-admin" element={<HomeAdmin />} />
+        <Route path="games-admin" element={<GamesAdmin />} />
+        <Route path="users-admin" element={<UsersAdmin />} />
+        <Route path="user-edition-admin" element={<UserEditionAdmin />} />
+        <Route path="game-edition-admin" element={<GameEditionAdmin />} />
+        <Route path="game-creation-admin" element={<GameCreationAdmin />} /> */}
     </Routes>
   </BrowserRouter>
 );

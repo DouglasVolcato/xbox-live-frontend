a80317d0a98d08e2f5aa1669d2ae300af62da89c
Douglas Volcato <douglasvolcato@gmail.com>
2023-02-02 15:27:49 -0300

    feat: implement user edition page navigation

--- src/main/react-adapter/pages/usersAdmin-page.tsx
+++ src/main/react-adapter/pages/usersAdmin-page.tsx
@@ -1,16 +1,18 @@
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { makeUsersAdminControllerFactory } from "../../factories/controllers/usersAdmin-controller-factory";
 import { HtmlComponent } from "../helpers/html-component-helper";
 
 export function UsersAdmin() {
   const usersAdminPage = makeUsersAdminControllerFactory();
+  const navigate = useNavigate();
 
-  function userPageNavitation() {
-    alert("User selected");
+  function navigateToUserEditon() {
+    navigate("/user-edition-admin");
   }
 
   useEffect(() => {
-    usersAdminPage.setUserList(userPageNavitation);
+    usersAdminPage.setUserList(navigateToUserEditon);
   });
 
   return <HtmlComponent component={usersAdminPage.renderPage()} />;

043c9ec3869ccc31ec61140f90065798bd56cdd2
Douglas Volcato <douglasvolcato@gmail.com>
2023-02-02 16:33:45 -0300

    feat: implement UserEditionAdmin page interaction

--- src/main/react-adapter/pages/userEditionAdmin-page.tsx
+++ src/main/react-adapter/pages/userEditionAdmin-page.tsx
@@ -1,12 +1,20 @@
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { makeUserEditionAdminControllerFactory } from "../../factories/controllers/userEditionAdmin-controller-factory";
 import { HtmlComponent } from "../helpers/html-component-helper";
 
 export function UserEditionAdmin() {
   const userEditionAdminPage = makeUserEditionAdminControllerFactory();
+  const navigate = useNavigate();
+
+  function navigateToUsersList() {
+    navigate("/users-admin");
+  }
 
   useEffect(() => {
     userEditionAdminPage.getUserInfo();
+    userEditionAdminPage.updateUser(navigateToUsersList);
+    userEditionAdminPage.deleteUser(navigateToUsersList);
   });
 
   return <HtmlComponent component={userEditionAdminPage.renderPage()} />;
